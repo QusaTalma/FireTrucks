4bef2458d6474674588d47e4efec9704d9abaf7a
Andy Sheehan <asheehan@Andrews-MacBook-Pro.local>
2014-04-18 00:11:26 -0500

    Commited assets that I've been working on

---
+++ Assets/EntityData/EDFiretruck.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EDFiretruck{
+	Vector3 position;
+	Vector3 destination;
+	float speed;
+
+	public float GetSpeed(){
+		return speed;
+	}
+
+	public void SetSpeed(float speed){
+		this.speed = speed;
+	}
+
+	public void SetPosition(Vector3 position){
+		this.position = position;
+	}
+
+	public Vector3 GetPosition(){
+		return position;
+	}
+
+	public void SetDestination(Vector3 destination){
+		this.destination = destination;
+	}
+
+	public Vector3 GetDestination(){
+		return destination;
+	}
+
+	public void Update(float deltaTime){
+		position = Vector3.Lerp (position, destination, speed * deltaTime);
+	}
+}
---
+++ Assets/EntityGraphics/EGFiretruck.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EGFiretruck : MonoBehaviour {
+	EDFiretruck _firetruck;
+	Vector3 position;
+	Vector3 destination;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		_firetruck = new EDFiretruck ();
+	}
+
+	void Update(){
+		float deltaTime = Time.deltaTime;
+		float xDiff = destination.x - transform.position.x;
+		float zDiff = destination.z - transform.position.z;
+		float angle = Mathf.Atan2 (zDiff, xDiff);
+		float dist = Vector3.Distance (position, destination);
+
+		if (dist > speed*deltaTime) {
+			transform.Translate (Mathf.Cos (angle) * speed * deltaTime, 0, Mathf.Sin (angle) * speed * deltaTime);
+		} else {
+			speed = 0;
+			transform.position = destination;
+		}
+
+		position = transform.position;
+	}
+	
+	public void SetPosition(Vector3 position){
+		this.position = position;
+	}
+
+	public void SetDestination(Vector3 destination){
+		this.destination = destination;
+	}
+}
