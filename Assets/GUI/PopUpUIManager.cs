d1123d2b7621cdeb508301a7345aaad8a2569162
Andy Sheehan <asheehan@Andrews-MacBook-Pro.local>
2014-11-25 14:23:33 -0600

    Created and added placeholder assets for NPCs

---
+++ Assets/GUI/PopUpUIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUpUIManager : MonoBehaviour {
+	public GameObject fireChiefImage;
+	public GameObject policeChiefImage;
+	public GameObject mayorImage;
+
+	void Start(){
+		fireChiefImage.SetActive (false);
+		policeChiefImage.SetActive (false);
+		mayorImage.SetActive (false);
+	}
+
+	void OnGUI(){
+
+	}
+}
