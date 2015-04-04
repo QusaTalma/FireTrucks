6110475a8a79466999746abbfd2bc71140363ea6
Chris Prunotto <ChrisPrunotto@gmail.com>
2015-04-03 20:23:40 -0500

    Wwise Implementation
    Wwise introduced to the hierarchy.

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FX_FIRE = 3610873635U;
+        static const AkUniqueID PLAY_FX_FIRESPAWN = 640836156U;
+        static const AkUniqueID PLAY_FX_FIRETRUCKSPAWN = 234833977U;
+        static const AkUniqueID PLAY_MX_GAMEPLAY = 368551784U;
+        static const AkUniqueID REMOVE_MX_GAMEPLAY_LPF = 228867509U;
+        static const AkUniqueID SET_MX_GAMEPLAY_LPF = 135109471U;
+        static const AkUniqueID SET_PARAMETER_FIRE_INTENSITY = 2129491064U;
+        static const AkUniqueID STOP_MX_GAMEPLAY = 3165009754U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PARAMETER_FIRE_INTENSITY = 1806580411U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GAMEPLAYBANK = 1822725939U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MX_GAMEPLAY = 2655229457U;
+        static const AkUniqueID SFX_GAMEPLAY = 3587590657U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
